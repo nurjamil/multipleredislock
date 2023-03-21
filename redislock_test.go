9cca66a15164a048f03d43465f76848d994c571a
Moladin <nurjamil1996@gmail.com>
2023-03-21 11:40:30 +0700

    delete option in refresh

--- redislock_test.go
+++ redislock_test.go
@@ -178,7 +178,7 @@ func TestLock_Refresh(t *testing.T) {
 	assertTTL(t, lock, time.Hour)
 
 	// update TTL
-	if err := lock.Refresh(ctx, time.Minute, nil); err != nil {
+	if err := lock.Refresh(ctx, time.Minute); err != nil {
 		t.Fatal(err)
 	}
 
@@ -196,7 +196,7 @@ func TestLock_Refresh_expired(t *testing.T) {
 
 	// try releasing
 	time.Sleep(10 * time.Millisecond)
-	if exp, got := ErrNotObtained, lock.Refresh(ctx, time.Minute, nil); !errors.Is(got, exp) {
+	if exp, got := ErrNotObtained, lock.Refresh(ctx, time.Minute); !errors.Is(got, exp) {
 		t.Fatalf("expected %v, got %v", exp, got)
 	}
 }
